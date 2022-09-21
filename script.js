8b1e2ced6ad2b91bcecfbdda92becf3943bcf452
edufelibugm <106450903+edufelibugm@users.noreply.github.com>
2022-09-21 10:52:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('menu-btn').addEventListener('click', () => {
+    document.querySelector('.nav-bar-menu').classList.toggle('desktop');
+})
+document.getElementById('close-menu').addEventListener('click', () => {
+    document.querySelector('.nav-bar-menu').classList.toggle('desktop');
+})
